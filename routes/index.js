8e4834962f96e19862f7b452d08d9bf267bf9cb2
rfdomingues98 <rfdomingues98@gmail.com>
2020-06-05 03:38:34 +0100

    Added login form, signup page, customer dashboard, fixed bugs on navbar and footer and added some /users routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AVEIRO|market - You buy, we' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'AVEIRO|market - You buy, we deliver'
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,48 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Test'})
+router.get('/', function (req, res, next) {
+  res.redirect("../");
+});
+
+router.get('/signup', function (req, res, next) {
+  let ctx = {
+    title: 'AVEIRO|market - You buy, we deliver',
+  }
+  res.render('signup', ctx);
+});
+
+router.get('/signup/seller', function (req, res, next) {
+  let ctx = {
+    title: 'AVEIRO|market - You buy, we deliver',
+  }
+  res.render('signup_seller', ctx);
+});
+
+router.get('/dashboard', function (req, res, next) {
+  let ctx = {
+    title: 'AVEIRO|market - You buy, we deliver',
+    user: {
+      name: "John Doe",
+      order_history: [{
+          order_no: 123456789,
+          date: "Jan 12, 2020",
+          state: "On-Hold"
+        },
+        {
+          order_no: 123456789,
+          date: "Jan 12, 2020",
+          state: "On-Hold"
+        },
+        {
+          order_no: 123456789,
+          date: "Jan 12, 2020",
+          state: "On-Hold"
+        }
+      ],
+    }
+  }
+  res.render('dashboard', ctx);
 });
 
 module.exports = router;
